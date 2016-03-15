c22ec1cf48c99a998cf4bde0da50087c4e84e2e7
prio101 <miprio101@gmail.com>
2016-03-16 00:47:52 +0600

    Added Migration for the contacts table

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  
+    # self.table_name = "contact_tables" 
+
+
 end
--- db/migrate/20160313195545_create_contacts.rb
+++ db/migrate/20160313195545_create_contacts.rb
@@ -1,6 +1,19 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      
+      t.string   :first_name
+      t.string   :last_name
+      t.string   :email
+      t.integer  :phone
+      t.string   :present_address
+      t.string   :permanent_address
+      t.text     :about
+      t.string   :facebook_link
+      t.string   :google_link
+      t.string   :twitter_link
+      t.string   :portfolio_img
+      t.string   :cover_pic
 
       t.timestamps null: false
     end
